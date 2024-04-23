ef131b9d8bf06088f7fdf83849c4d9ad17eefd1d
Garima Chib <garimachib01@gmail.com>
2024-04-23 20:22:35 +0000

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='oa_search_cite',
+      version='0.0.1',
+      description='OpenAlex Search',
+      maintainer='Garima Chib',
+      maintainer_email='gchib@andrew.cmu.edu',
+      license='MIT',
+      packages=['oa_search_cite'],
+      scripts=[],
+      long_description='OpenAlex keyword search and retrieval of citation trends')
